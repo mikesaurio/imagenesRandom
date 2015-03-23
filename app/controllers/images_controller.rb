02fc1676e99481dc06af720855ab7033f0e5d5ad
mikesaurio <mikesaurio@gmail.com>
2015-03-22 22:06:13 -0600

    imagenes random

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,22 @@
 class ImagesController < ApplicationController
+   before_action :set_arreglo, only: [:index]
+
   def index
+  #.sample
+      if params[:commit]
+          @ganador =  @arreglo.sample
+      else
+           @arreglo
+      end
+  
   end
+
+
+private
+     def set_arreglo
+      @arreglo = ['aleman.jpg', 'arellano.jpg', 'avalos.jpg', 'caballero.jpg', 'caldino.jpg', 'castro.jpg', 'farfan.jpg', 'fernandez.jpg', 'galardo.jpg', 'garcia.jpg', 
+      'gonzalez.jpg', 'hernandez.jpg', 'iturbide.jpg', 'juarez.jpg', 'limon.jpg', 'lua.jpg', 'miguel.jpg', 'millan.jpg', 'ortiz.jpg', 
+      'pareda.jpg', 'perez.jpg', 'ramirez.jpg', 'redriguez.jpg', 'sanchez.jpg', 'tenorio.jpg', 'viurquis.jpg', 'zavala.jpg']
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'images#index'
+
+ resources :images
 end
