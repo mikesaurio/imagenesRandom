e69003154e41c555c428d002782b9a5fac7a5907
mikesaurio <mikesaurio@gmail.com>
2015-03-22 23:43:17 -0600

    cambio a pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150323041328) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "wins", force: :cascade do |t|
     t.text     "foto"
     t.datetime "created_at", null: false
